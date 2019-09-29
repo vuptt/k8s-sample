72e94880363e9dbc0baf177867ba52c140cf677e
Vu Phan <ptvu.it@gmail.com>
2019-09-30 00:15:57 +0700

    add fluent bit

--- cluster-bionic/export-cert.sh
+++ cluster-bionic/export-cert.sh
@@ -2,3 +2,5 @@ mkdir -p certs
 grep 'client-certificate-data' $HOME/.kube/config | awk '{print $2}' | base64 -d | openssl x509 -out certs/client.crt
 grep 'certificate-authority-data' $HOME/.kube/config | awk '{print $2}' | base64 -d | openssl x509 -out certs/ca.crt
 grep 'client-key-data' $HOME/.kube/config | awk '{print $2}' | base64 -d >> certs/client.key
+
+
